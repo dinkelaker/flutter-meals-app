a2a73b8e1522e821830ad23f6384c8924b7f76fa
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-26 10:00:57 +0200

    Improved MealItem

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -20,6 +20,23 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   }) : super(key: key);
 
+  String get complexityText {
+    switch(complexity) {
+      case Complexity.Simple: return 'Simple'; break;
+      case Complexity.Challenging: return 'Callenging'; break;
+      case Complexity.Hard: return 'Hard'; break;
+      default: return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch(affordability) {
+      case Affordability.Affordable: return 'Affordable'; break;
+      case Affordability.Pricey: return 'Pricey'; break;
+      case Affordability.Luxurious: return 'Expensive'; break;
+    }
+  }
+
   void selectMeal(BuildContext ctxt) {
     // Navigator.of(ctxt).pushNamed(Screen.routeName, arguments: {
     //   'id': id,
@@ -51,8 +68,61 @@ class MealItem extends StatelessWidget {
                   child: Image.network(imageUrl,
                       height: 250, width: double.infinity, fit: BoxFit.cover),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                )
               ],
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.schedule,
+                      ),SizedBox(width: 6),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.work,
+                      ),SizedBox(width: 6),
+                      Text('$complexityText'),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.attach_money,
+                      ),SizedBox(width: 6),
+                      Text('$affordabilityText'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
