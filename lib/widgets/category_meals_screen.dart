543ef662e095a3b155e60df19107dfc6d3c2e18d
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-26 09:19:55 +0200

    Category meal screen with a list of recipes

--- lib/widgets/category_meals_screen.dart
+++ lib/widgets/category_meals_screen.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
-  
+
   @override
   Widget build(BuildContext context) {
-    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String,String>;
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
-
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(child: Text('CategoryMeals!')),
+      body: ListView.builder(
+        itemBuilder: (ctxt, index) {
+          return Text(categoryMeals[index].title);
+        },
+        itemCount: categoryMeals.length,
+      ),
     );
   }
 }
