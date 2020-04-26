ca872365de1da653ada0f85237a55d194a5aabd5
Tom Dinkelaker <tom.dinkelaker@gmail.com>
2020-04-26 08:05:30 +0200

    Categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/categories_screen.dart';
+
+import 'dummy_data.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,7 +12,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            body2: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            title: TextStyle(
+              fontSize: 24,
+              fontFamily: 'RobotoCondensed',
+            )),
       ),
       home: MyHomePage(),
     );
@@ -16,21 +34,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-    @override
+  @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
-    );
+    return CategoriesScreen(categories: DUMMY_CATEGORIES);
+    //  Scaffold(
+    //   appBar: AppBar(
+    //     title: const Text('Deli Meals'),
+    //   ),
+    //   body: Center(
+    //     child: CategoriesScreen(categories: DUMMY_CATEGORIES),
+    //   ),
+    // );
   }
 }
---
+++ lib/widgets/categories_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+import 'category_item.dart';
+
+
+class CategoriesScreen extends StatelessWidget {
+  final List<Category> categories;
+
+  const CategoriesScreen({Key key, this.categories}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli Meals'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(15),
+        children: 
+
+          categories.map(
+            (catData) => CategoryItem(
+                catData.title, 
+                catData.color,
+              )
+          )
+          .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
